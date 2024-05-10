236ee5e73ff45f1ca80a5de68febfd11ba5d748c
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-10 23:45:46 +0530

    added translate.js

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,7 @@
 import * as ping from "./ping";
 import * as jokes from "./jokes";
+import * as translate from "./translate";
 
 export const commands = {
-    ping, jokes
+    ping, jokes, translate
 };
---
+++ src/commands/translate.ts
@@ -0,0 +1,37 @@
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { config } from "../config";
+
+export const data = new SlashCommandBuilder()
+    .setName("translate")
+    .setDescription("Send the translation to English for the input text")
+    .addStringOption(option =>
+        option
+            .setName("text")
+            .setDescription("Enter the text to be translated")
+            .setRequired(true)
+    );
+
+export async function execute(interaction: CommandInteraction) {
+    await interaction.deferReply();
+    const options: AxiosRequestConfig = {
+        method: 'POST',
+        url: `https://google-translate124.p.rapidapi.com/google-translation/`,
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': config.X_RapidAPI_Key,
+            'X-RapidAPI-Host': 'google-translate124.p.rapidapi.com'
+        },
+        data: {
+            from_lang: 'auto',
+            to_lang: 'english',
+            text: `${interaction.options.get('text')}`
+        }
+    };
+    try {
+        const response: AxiosResponse = await axios.request(options);
+        await interaction.editReply(`Translation of '${interaction.options.get('text')}' is - '${response.data.Response}'`);
+    } catch (err) {
+        console.log(err);
+    }
+}
