2f76131430450049637479895fa086958f280c72
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-11 00:17:14 +0530

    added missing imports

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,7 +2,9 @@ import * as ping from "./ping";
 import * as jokes from "./jokes";
 import * as translate from "./translate";
 import * as info from "./info";
+import * as truthdate from "./truthdare";
+import * as lyrics from "./lyrics";
 
 export const commands = {
-    ping, jokes, translate, info
+    ping, jokes, translate, info, lyrics, truthdate
 };
