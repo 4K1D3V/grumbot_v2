bcc5e34f33b36d5427df15ea7a0a80df6ba9f996
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-10 23:28:16 +0530

    added messageCreate event and a command to deploy commands

--- src/bot.ts
+++ src/bot.ts
@@ -5,15 +5,14 @@ import { config } from './config';
 import express from 'express';
 
 const client = new Client({
-    intents: ["Guilds", "GuildMessages", "DirectMessages"],
+    intents: ["Guilds", "GuildMessages", "DirectMessages", "MessageContent"],
 });
 
 client.once("ready", async () => {
     console.log("Discord bot is ready! 🤖");
-    await deployCommands();
 });
 
-client.on("guildCreate", async (guild) => {
+client.on("guildCreate", async () => {
     await deployCommands();
 });
 
@@ -35,4 +34,16 @@ const port = config.PORT;
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}!`);
+})
+
+//Message Create Events
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+    if (message.content.toLowerCase() === "!deploy-commands") {
+        if (message.author.id === "1216042650096898189") {
+            await deployCommands();
+            message.reply("Commands deployed!");
+        } else message.reply("You do not have permission to do this!");
+
+    }
 })
