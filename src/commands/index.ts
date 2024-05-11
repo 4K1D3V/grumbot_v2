7e78646fd1e140bb365a96c77fbbae057b66e848
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-11 13:48:03 +0530

    removed banner slash command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -5,8 +5,7 @@ import * as info from "./info";
 import * as lyrics from "./lyrics";
 import * as truthdare from "./truthdare";
 import * as avatar from "./avatar";
-import * as banner from "./banner"
 
 export const commands = {
-    ping, jokes, translate, info, lyrics, truthdare, avatar, banner
+    ping, jokes, translate, info, lyrics, truthdare, avatar
 };
