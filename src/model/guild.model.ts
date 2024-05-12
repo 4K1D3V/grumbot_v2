5573d73f61480378cd4c3c9f1b9ecf197f254490
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-12 20:07:43 +0530

    updated guild model

--- src/model/guild.model.ts
+++ src/model/guild.model.ts
@@ -3,5 +3,5 @@ import { RowDataPacket } from "mysql2";
 export default interface Guild extends RowDataPacket {
     guild_id?: string,
     guild_name?: string,
-    command_prefix?: string & { lenght: 1 },
+    command_prefix?: string,
 }
