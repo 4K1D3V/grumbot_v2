92cbac68a8bb6b21c2a7228e06bab1f7f81ed2ad
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-13 15:27:28 +0530

    added bot image in config

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,9 @@ const {
   MYSQL_DATABASE,
   MYSQL_PORT
 } = process.env;
+
 const STAFF_USER_ID: String[] = ["1216042650096898189"];
+const BOT_IMAGE = "https://cdn.discordapp.com/avatars/1225394097280253982/d5326ca03c8b52f441173368b7e4e0ed?size=1024"
 
 if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
   throw new Error("Missing environment variables");
@@ -33,5 +35,6 @@ export const config = {
   MYSQL_USER,
   MYSQL_PASSWORD,
   MYSQL_DATABASE,
-  MYSQL_PORT
+  MYSQL_PORT,
+  BOT_IMAGE
 };
