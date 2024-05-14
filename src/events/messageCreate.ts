650986db2ccb4d2e9da280b22f4a8bc4a58648f4
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-14 17:35:54 +0530

    added guild staff map to message create event

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -16,7 +16,7 @@ export async function execute(message: Message<boolean>) {
 
     // Reply to !reload to deploy bot commands to discord
     if (message.content.toLowerCase() === `${prefix}reload`) {
-        if (config.STAFF_USER_ID.includes(message.author.id)) {
+        if (allGuildsMap.guildStaffUserIdMap.get(message.guildId!)?.includes(message.author.id)) {
             const numberOfCommands = await deployCommands()
                 .catch(error => {
                     console.error(error);
