95aa701e78501b6179c785d7302cc434cc4b15e3
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-14 21:27:12 +0530

    fixed condition for changeprefix command

--- src/commands/admin/changeprefix.ts
+++ src/commands/admin/changeprefix.ts
@@ -7,7 +7,7 @@ import allGuildsMap from "../../bot";
 export const data = new SlashCommandBuilder()
     .setName("changeprefix")
     .setDescription("Changes the command prefix for the guild")
-    .addStringOption(option => 
+    .addStringOption(option =>
         option
             .setName("prefix")
             .setDescription("The command prefix")
@@ -15,19 +15,21 @@ export const data = new SlashCommandBuilder()
                 { name: "!", value: "!" },
                 { name: "?", value: "?" },
                 { name: "-", value: "-" },
-                { name: "~", value: "~"}
+                { name: "~", value: "~" }
             )
             .setRequired(true)
     )
 
 export async function execute(interaction: CommandInteraction) {
     await interaction.deferReply();
-    if (!allGuildsMap.guildStaffUserIdMap.get(interaction.guildId!)?.includes(interaction.user.id) || interaction.user.id !== interaction.guild?.ownerId) {
-        await interaction.editReply({content: "You don't have permission to run this command!" });
-        return;
+    if (interaction.user.id !== interaction.guild?.ownerId) {
+        if (!allGuildsMap.guildStaffUserIdMap.get(interaction.guildId!)?.includes(interaction.user.id)) {
+            await interaction.editReply({ content: "You don't have permission to run this command!" });
+            return;
+        }
     }
     const commandPrefix = interaction.options.get("prefix")?.value;
-    const guild =  {
+    const guild = {
         guild_id: interaction.guildId!,
         guild_name: interaction.guild?.name,
         command_prefix: commandPrefix as string
