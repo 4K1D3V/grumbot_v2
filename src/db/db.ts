758a177e6631a3d34c79bbcd6f9de125f06c0042
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-15 21:47:06 +0530

    added sql connection disconnec handling

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,27 @@
 import mysql2 from "mysql2";
 import dbConfig from "./db.config";
 
-export default mysql2.createConnection({
+const connectionConfig = {
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DATABASE,
     port: 12435
-});
+}
+
+export var connection = mysql2.createConnection(connectionConfig);
+
+function handleDisconnet() {
+    connection = mysql2.createConnection(connectionConfig)
+}
+
+connection.on("error", (err) => {
+    console.log("DB Connection Closed. Attenpting Reconnect!");
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        handleDisconnet();
+    } else {
+        console.log("DB Connection Error!");
+    }
+})
+
+export default connection;
