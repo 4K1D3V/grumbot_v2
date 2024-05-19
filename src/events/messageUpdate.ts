cf3008bc15218737bdecd271be47efd3ab0e0f38
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-19 22:55:43 +0530

    fixed buged if condition, and added color to embed

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -10,20 +10,25 @@ import allGuildsMap from "../bot";
 export async function execute(oldMessage: Message<boolean>, newMessage: Message<boolean>) {
     const logsChannelId = allGuildsMap.guildLogsChannelMap.get(oldMessage.guildId!);
     const isLogsChannelSet = logsChannelId !== undefined;
+    const isLogsEnabled = allGuildsMap.guildIsLogsEnabledMap.get(oldMessage.guildId!);
     const logsChannel = isLogsChannelSet ? oldMessage.client.channels.cache.get(logsChannelId) as TextChannel : undefined;
-    if (!logsChannel && allGuildsMap.guildIsLogsEnabledMap.get(oldMessage.guildId!) === 0) return;
-    const messageEmbed = new EmbedBuilder();
-    messageEmbed
-        .setTitle("Message Edited")
-        .setDescription(`Messag edited by <@${oldMessage.author.id}> in <#${oldMessage.channel.id}>`)
-        .setTimestamp()
-        .setFooter({ text: "Grumbot", iconURL: `${config.BOT_IMAGE}` });
-    if (oldMessage.author.bot || newMessage.author.bot) return;
+    if (isLogsEnabled === 0) return;
+    else if (logsChannel === undefined) return;
     else {
-        messageEmbed.addFields(
-            { name: "Message Content", value: `Changed from ${oldMessage.content.substring(0, 450)} to ${newMessage.content.substring(0, 450)}`, inline: false },
-            { name: "Edited at", value: `${newMessage.editedAt}`, inline: false }
-        )
-        await logsChannel?.send({ content: "Message Edit Detected", embeds: [messageEmbed] });
+        const messageEmbed = new EmbedBuilder();
+        messageEmbed
+            .setTitle("Message Edited")
+            .setDescription(`Messag edited by <@${oldMessage.author.id}> in <#${oldMessage.channel.id}>`)
+            .setColor(0xFFFF00)
+            .setTimestamp()
+            .setFooter({ text: "Grumbot", iconURL: `${config.BOT_IMAGE}` });
+        if (oldMessage.author.bot || newMessage.author.bot) return;
+        else {
+            messageEmbed.addFields(
+                { name: "Message Content", value: `Changed from ${oldMessage.content.substring(0, 450)} to ${newMessage.content.substring(0, 450)}`, inline: false },
+                { name: "Edited at", value: `${newMessage.editedAt}`, inline: false }
+            )
+            await logsChannel?.send({ content: "Message Edit Detected", embeds: [messageEmbed] });
+        }
     }
 }
