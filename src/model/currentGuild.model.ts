6e9512b82dcf68a4d902d9000a1533c3a0c12fb5
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-30 16:19:37 +0530

    added logs variables in model

--- src/model/currentGuild.model.ts
+++ src/model/currentGuild.model.ts
@@ -5,5 +5,7 @@ export default interface CurrentGuild extends RowDataPacket {
     guild_name?: string,
     command_prefix?: string,
     logs_channel_id?: string,
-    is_logs_enabled?: number
+    is_logs_enabled?: number,
+    is_tickets_enabled?: number,
+    tickets_data?: string
 }
