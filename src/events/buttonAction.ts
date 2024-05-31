ca3082b122a874b3feb6de128fa31325ba4117e2
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-05-31 23:53:55 +0530

    updated button action (still pending)

--- src/events/buttonAction.ts
+++ src/events/buttonAction.ts
@@ -1,25 +1,30 @@
-import { ButtonInteraction, ChannelType, PermissionFlagsBits } from "discord.js";
+import { ButtonInteraction, ChannelType, EmbedBuilder, PermissionFlagsBits, PermissionsBitField } from "discord.js";
 import allGuildsMap from "../bot"
+import TicketData from "../model/ticketData.model";
 
 export async function execute(interaction: ButtonInteraction) {
     if (interaction.customId === "ticketButton") {
-        const ticketCategory = allGuildsMap.guildTicketsDataMap.get(interaction.guild?.id!);
+        const guildTicketData: TicketData = JSON.parse(allGuildsMap.guildTicketsDataMap.get(interaction.guild?.id!)!);
         try {
-            await interaction.guild?.channels.create({
+            const createdTicketChannel = await interaction.guild?.channels.create({
                 name: `ticket-${interaction.user.username}`,
-                type: ChannelType.GuildText,
+                type: ChannelType.GuildText
             }).then(channel => {
-                channel.setParent(`${ticketCategory}`);
-                channel.permissionOverwrites.set([
-                    {
-                        id: interaction.user.id,
-                        allow: [PermissionFlagsBits.SendMessages, PermissionFlagsBits.AddReactions, PermissionFlagsBits.AttachFiles, PermissionFlagsBits.ViewChannel, PermissionFlagsBits.ReadMessageHistory]
-                    },
-                    {
-                        id: interaction.guild?.roles.everyone.id as string,
-                        deny: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.SendMessages, PermissionFlagsBits.ReadMessageHistory]
-                    },
-                ])
+                channel.setParent(`${guildTicketData.ticketCategory}`);
+                channel.permissionOverwrites.create(interaction.guild?.roles.everyone.id as string, { ViewChannel: false, SendMessages: false, });
+                channel.permissionOverwrites.create(interaction.user.id, { ViewChannel: true, SendMessages: true, AttachFiles: true, SendVoiceMessages: true, EmbedLinks: true, AddReactions: true, UseExternalEmojis: true, ReadMessageHistory: true });
+                guildTicketData.ticketRoles.split(",").forEach(role => {
+                    channel.permissionOverwrites.create(role, { ViewChannel: true, UseApplicationCommands: true, ManageChannels: true, ManageMessages: true, ReadMessageHistory: true })
+                })
+                return channel;
+            })
+            const ticketEmbed = new EmbedBuilder()
+                .setTitle(`${interaction.guild?.name} Support`)
+                .setDescription("How can we help you?")
+                .setTimestamp(new Date())
+                .setFooter({ text: "Grumbot", iconURL: interaction.client.user.displayAvatarURL() });
+            await createdTicketChannel?.send({
+                embeds: [ticketEmbed]
             })
         } catch (err) {
             interaction.reply({
