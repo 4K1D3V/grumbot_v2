23338b5ffaa47e78cdda2216e0271cad6e97cb70
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2024-06-09 16:13:26 +0530

    fixed db error (maybe)

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,30 +1,42 @@
 import mysql2, { ConnectionOptions } from "mysql2";
 import dbConfig from "./db.config";
 
-const connectionConfig: ConnectionOptions = {
+const poolConfig: ConnectionOptions = {
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DATABASE,
     port: 12435,
-    connectTimeout: 180
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 }
 
-export var connection = mysql2.createConnection(connectionConfig);
+export const connection = mysql2.createConnection(poolConfig);
 
 function handleDisconnet() {
-    connection = mysql2.createConnection(connectionConfig);
-    connection.connect((err) => {
-        if (err) setTimeout(handleDisconnet, 2000);
-    });
-    connection.on("error", (err) => {
-        console.log("DB Connection Closed. Attempting Reconnect!");
-        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-            handleDisconnet();
-        }
+    connection.on('connection', (connection) => {
+        console.log("New DB Connection Estabilished!");
+        connection.on('error', (err: any) => {
+            console.log(`DB Connection Error - ${err}`);
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.log("Connection lost; attempting reconnect!");
+                handleDisconnet();
+            }
+        })
     })
 }
 
+function keepAlive() {
+    setInterval(() => {
+        connection.query('SELECT 1', (err) => {
+            if (err) console.log(`Keep alive query failed! - ${err}`)
+            else console.log(`Keep alive query success!`)
+        });
+    }, 10_000);
+}
+
 handleDisconnet();
+keepAlive();
 
 export default connection;
